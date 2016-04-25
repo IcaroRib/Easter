a7a0f7edb734ce03cbad07ede6e33dec86ee630d
FrancoisM <francois_michell95@hotmail.com>
2016-04-24 21:44:33 -0300

    Criação do projeto no AndroidStudio

---
+++ Easter/app/src/main/java/bsi/pp_2016_1/easter/Domain/ComentaryAvaliation.java
@@ -0,0 +1,23 @@
+package bsi.pp_2016_1.easter.Domain;
+
+
+public class ComentaryAvaliation {
+
+    private int idUser;
+    private boolean avaliation;
+
+    public int getIdUser() {
+        return idUser;
+    }
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public boolean getAvaliation() {
+        return avaliation;
+    }
+    public void setAvaliation(boolean avaliation) {
+        this.avaliation = avaliation;
+    }
+
+}
---
+++ Easter/app/src/main/java/bsi/pp_2016_1/easter/Domain/Reference.java
@@ -0,0 +1,29 @@
+package bsi.pp_2016_1.easter.Domain;
+public class Reference {
+
+    private int referenceId;
+    private String referenceTitle;
+    private String referenceType;
+
+    public int getReferenceId() {
+        return referenceId;
+    }
+    public void setReferenceId(int referenceId) {
+        this.referenceId = referenceId;
+    }
+
+    public String getReferenceTitle() {
+        return referenceTitle;
+    }
+    public void setReferenceTitle(String referenceTitle) {
+        this.referenceTitle = referenceTitle;
+    }
+
+    public String getReferenceType() {
+        return referenceType;
+    }
+    public void setReferenceType(String referenceType) {
+        this.referenceType = referenceType;
+    }
+
+}
